e1bc69017076eec9eddaf4d3a65c3ac2d69bc5e4
Karl Zylinski <karl@zylinski.se>
2025-06-06 01:02:52 +0200

    shader

---
+++ src/shader.hlsl
@@ -0,0 +1,24 @@
+cbuffer ConstantBuffers : register(b0) {
+	float4x4 mvp;
+	float nums[64];
+};
+struct PSInput {
+	float4 position : SV_POSITION;
+	float4 color : COLOR;
+};
+PSInput VSMain(float4 position : POSITION0, float4 color : COLOR0, uint v : SV_VertexID) {
+	PSInput result;
+
+	if (v == 10) {
+		position = float4(0, nums[v-10], 0, 1);
+	}
+
+	result.position = mul(position, mvp);
+
+
+	result.color = color;
+	return result;
+}
+float4 PSMain(PSInput input) : SV_TARGET {
+	return input.color;
+};
